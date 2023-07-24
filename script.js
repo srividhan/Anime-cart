2ddcbb47b71547c9db8c0c03e0c3adf620bdc458
srividhan <125474325+srividhan@users.noreply.github.com>
2023-07-24 20:52:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+
+function fun1(){
+    document.getElementById("book").style="background-image:url(https://i.pinimg.com/736x/8e/05/5e/8e055ee601ee886559792445f28a4765.jpg);"
+    document.getElementById("epi").innerHTML="Epsidoes : 220";
+    document.getElementById("ses").innerHTML="Seasons : 5";
+    document.getElementById("naruto").innerHTML="Naruto";
+    document.getElementById("price").innerHTML="Price:- $200,000";
+    if(document.getElementById("butt").innerHTML="Your item is added to cart"){
+        document.getElementById("butt").innerHTML="Add to cart";
+    }
+    document.getElementById("butt").style="margin-top: 0.1%";
+}
+function fun2(){
+    document.getElementById("book").style="background-image:url(https://i.pinimg.com/originals/0b/bb/a5/0bbba5e82c245cfcf9008f3068bc4874.jpg);"
+    document.getElementById("epi").innerHTML="Epsidoes : 500";
+    document.getElementById("ses").innerHTML="Season : 21";
+    document.getElementById("naruto").innerHTML="Naruto: Shippuden";
+    document.getElementById("price").innerHTML="Price:- $500,000";
+    if(document.getElementById("butt").innerHTML="Your item is added to cart"){
+        document.getElementById("butt").innerHTML="Add to cart";
+    }
+    document.getElementById("butt").style="margin-top: 0.1%";
+}
+function fun3(){
+    document.getElementById("book").style="background-image:url(https://quick-step.pt/10-30-50pcs-naruto-graffiti-adesivos-anime-decorativos_images/5-share_5962.jpg);"
+    document.getElementById("epi").innerHTML="Movies : 3";
+    document.getElementById("ses").innerHTML=" ";
+    document.getElementById("naruto").innerHTML="Naruto Movies";
+    document.getElementById("price").innerHTML="Price:- $250,000";
+    if(document.getElementById("butt").innerHTML="Your item is added to cart"){
+        document.getElementById("butt").innerHTML="Add to cart";
+    }
+    document.getElementById("butt").style="margin-top: 5.8%";
+}
+function fun4(){
+    document.getElementById("book").style="background-image:url(https://i.pinimg.com/1200x/ec/51/74/ec5174058529151f99ae48e8bf42b3a4.jpg);"
+    document.getElementById("epi").innerHTML="Movies : 7";
+    document.getElementById("ses").innerHTML=" ";
+    document.getElementById("naruto").innerHTML="Naruto: Shippuden Movies";
+    document.getElementById("price").innerHTML="Price:- $700,000";
+    if(document.getElementById("butt").innerHTML="Your item is added to cart"){
+        document.getElementById("butt").innerHTML="Add to cart";
+    }
+    document.getElementById("butt").style="margin-top: 5.8%";
+}
+function fun5(){
+    document.getElementById("butt").innerHTML="Your item is added to cart";
+}
